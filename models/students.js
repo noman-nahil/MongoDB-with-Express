db0726fa399520fe30a6749054867fb3c2265268
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-06 14:44:27 +0600

    Defining Model

---
+++ models/students.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose');
+
+const Student = model('Student', Schema({
+    name: { type: String },
+    age: { type: Number, min: 0 },
+    hobbies: {
+        type: Array,
+        of: String,
+        validate: {
+            validator: (value) => value.length > 0,
+            message: "There must be at least one hobby!",
+        },
+    }
+}));
+exports.Student = Student;
--- routers/studentRouters.js
+++ routers/studentRouters.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const { Student } = require('../models/students')
 
 //Named Function
 const studentList = (req, res) => { }
