85262b851d861c827b020851190c59bd00a5e96e
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-06 14:33:05 +0600

    structuring project

---
+++ App.js
@@ -0,0 +1,26 @@
+//Express
+const express = require('express');
+const app = express();
+const studentRouter = require('./routers/studentRouters')
+
+
+app.use(express.json());
+app.use('/api/students', studentRouter);
+
+
+
+//
+/*
+app.get('/api/students', studentList);
+app.post('/api/students', newStudent);
+app.get('/api/students/:id', studentDetails);
+app.put('/api/students/:id', updateStudent);
+app.delete('/api/students/:id', deleteStudent);*/
+
+//
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('App listening on port ' + port + '!');
+});
+
---
+++ routers/studentRouters.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Named Function
+const studentList = (req, res) => { }
+
+
+const newStudent = (req, res) => {
+    const student = req.body;
+
+}
+
+const studentDetails = (req, res) => {
+    const id = parseInt(req.params.id);
+    //console.log(id)
+
+}
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    //console.log(id)
+    const updateInfo = req.body;
+
+}
+
+const deleteStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    //console.log(id)
+
+}
+
+//Refactoring the routes
+router.route('/')
+    .get(studentList)
+    .post(newStudent);
+
+router.route('/:id')
+    .get(studentDetails)
+    .put(newStudent).
+    delete(deleteStudent);
+
+module.exports = router;
