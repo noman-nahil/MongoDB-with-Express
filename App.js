a1f00d6d9da93ed10036acfaf240b482e397b9cb
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-07 16:56:46 +0600

    Authentic user

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const studentRouter = require('./routers/studentRouters')
 const userRouter = require('./routers/userRouter')
+const authRouter = require('./routers/authRouter')
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/my-students-2', {
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/my-students-2', {
 app.use(express.json());
 app.use('/api/students', studentRouter);
 app.use('/register', userRouter);
+app.use('/login', authRouter);
 
 
 const port = 3000;
---
+++ routers/authRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const { User } = require('../models/users');
+
+
+const authUser = async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid email or password!");
+
+    const validUser = await bcrypt.compare(req.body.password, user.password);
+    if (!validUser) return res.status(400).send("Invalid email or password!");
+    res.send('Login Successful!');
+}
+
+router.route('/')
+    .post(authUser);
+
+
+
+module.exports = router;
